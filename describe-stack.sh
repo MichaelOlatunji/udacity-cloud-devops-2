292ba62da1c4251a47fbb12a3898b522a41ad143
Michael Olatunji <tundexmike@gmail.com>
2020-05-16 20:08:29 +0100

    list and describe stack shell files added

---
+++ describe-stack.sh
@@ -0,0 +1,4 @@
+aws cloudformation describe-stacks --stack-name $1
+
+# describe-stack-events --stack-name $stackname : describes stacks events
+# get-template --stack-name $stackname : retrieves deleted templates for last 90days
---
+++ list-stack.sh
@@ -0,0 +1,5 @@
+aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE
+
+# use aws cloudformation list-stacks to get information on all stacks and the ones deleted from the past 90days
+# --stack-status-filter can take CREATE_COMPLETE,CREATE_IN_PROGRESS, CREATE_FAILED DELETE_COMPLETE
+# --list-stack-resources --stack-name $stackname: lists stack resources
